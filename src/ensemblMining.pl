a9a5d813de6a6cd97cff9ed00d44db9f9c6270bd
Fran Abad <francisco.abad@um.es>
2016-05-02 13:04:05 +0200

    Output file and extra columns on csv

--- src/ensemblMining.pl
+++ src/ensemblMining.pl
@@ -2,29 +2,50 @@ use strict;
 use warnings;
 use Bio::EnsEMBL::Registry;
 
-my $registry = 'Bio::EnsEMBL::Registry';
+#Get output file from input parameters
+my $output = "";
+if (scalar @ARGV == 1){
+	$output = $ARGV[0];
+} else {
+	print "Usage: perl ensemblMining.pl outputFile";
+	exit;
+}
+open(my $fh, '>', $output) or die "Could not open file '$output' $!";
+print "Results will be printed in $output";
 
+# Registry configuration
+my $registry = 'Bio::EnsEMBL::Registry';
 $registry->load_registry_from_db(
     -host => 'ensembldb.ensembl.org',
     -user => 'anonymous',
 );
+# Set the flag to make sure that the database connection is dropped if not being used on each database.
+$registry->set_disconnect_when_inactive();
+# Set the flag to make sure that the database connection is not lost before it's used. This is useful for long running jobs (over 8hrs).
+$registry->set_reconnect_when_lost();
 
 
-#get the adaptor to get the Transcript, slices and transcript variation from the database
+# Get the adaptor to get the Transcript, slices and transcript variation from the database
 my $transcript_adaptor = $registry->get_adaptor( 'homo_sapiens', 'core', 'transcript' );
 my $slice_adaptor = $registry->get_adaptor( 'Human', 'Core', 'Slice' );
 my $trv_adaptor = $registry->get_adaptor( 'homo_sapiens', 'variation', 'transcriptvariation' );
 
-my @chromosomes = ('Y');
+# Chromosomes to be treated
+my @chromosomes = qw(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 X Y);
+
+# Sequence Ontology terms
 my @so_terms = ('start_lost');
 
 
+# Print csv header
+my @header = qw(CHROMOSOME GENE_ID GENE_NAME TRANSCRIPT_ID VARIATION_NAME MINOR_ALLELE_FREQUENCY CODON_CHANGE AMINOACID_CHANGE  CONSEQUENCE SO_TERM SIFT POLYPHEN);
+print $fh join(",", @header) . "\n";
 
-my @header = qw(GENE CODON_CHANGE AMINOACID_CHANGE  CONSEQUENCE SO_TERM SIFT POLYPHEN);
-print join(",", @header) . "\n";
+# For each chromosome, get its variations with specified so terms.
 foreach my $chromosome (@chromosomes) {
 	get_variations_by_chromosome_so_terms($chromosome, \@so_terms);
 }
+close($fh);
 
 # Get variations from chromosome (param 1) with the so term (param 2).
 # param 1 -> Chromosome name
@@ -38,14 +59,15 @@ sub get_variations_by_chromosome_so_terms {
 	# Get variation in transcripts with so terms
 	my $trvs = $trv_adaptor->fetch_all_by_Transcripts_SO_terms($transcript, $so_terms);
 
-	
+	my $count = 1;
 	foreach my $tv ( @{$trvs} ) {
+	    print "Chromosome $chromosome\t Transcript Variation: $count/" . scalar @{$trvs} . "\n";
+	    $count = $count + 1;
 	    my $tvas = $tv->get_all_alternate_TranscriptVariationAlleles();
-
+	    my $minor_allele_frequency = $tv->variation_feature->minor_allele_frequency ? $tv->variation_feature->minor_allele_frequency : '-';
 	    foreach my $tva ( @{$tvas} ) {
 		my @ensembl_consequences;
 		my @so_consequences;
-
 		my $ocs = $tva->get_all_OverlapConsequences();
 
 		foreach my $oc ( @{$ocs} ) {
@@ -55,21 +77,26 @@ sub get_variations_by_chromosome_so_terms {
 
 		my $sift     = $tva->sift_prediction;
 		my $polyphen = $tva->polyphen_prediction;
-		
-		print $tv->transcript->get_Gene->stable_id, ",",  $tva->transcript_variation->get_reference_TranscriptVariationAllele->codon,
-		  "/",$tva->codon,
-		  ",", $tva->pep_allele_string,
-		  ",", join( "-", @ensembl_consequences ),
-		  ",", join( "-", @so_consequences );
 
+		print $fh $chromosome . "," . $tv->transcript->get_Gene->stable_id . "," . 
+		  $tv->transcript->get_Gene->external_name . "," . 
+		  $tv->transcript->display_id . "," .
+		  $tv->variation_feature->variation_name . "," .
+		  $minor_allele_frequency . "," .
+		  $tva->transcript_variation->get_reference_TranscriptVariationAllele->codon . "/" . $tva->codon . "," . $tva->pep_allele_string .
+		  "," . join( "-", @ensembl_consequences ) .
+		  "," . join( "-", @so_consequences );
+
+		print $fh ",";
 		if ( defined($sift) ) {
-		    print ",$sift";
+		    print $fh "$sift";
 		}
+		print $fh ",";
 		if ( defined($polyphen) ) {
-		    print ",$polyphen";
+		    print $fh "$polyphen";
 		}
 
-		print "\n";
+		print $fh "\n";
 	    }
 	}
 }
