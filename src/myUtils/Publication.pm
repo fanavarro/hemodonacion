75ec665709f83b228c681f69b4fb00452b9dc5bc
fanavarro <francisco.abad@um.es>
2016-05-19 13:23:26 +0200

    find link by doi

--- src/myUtils/Publication.pm
+++ src/myUtils/Publication.pm
@@ -2,7 +2,6 @@ package myUtils::CsvManager;
 use strict;
 use warnings;
 use LWP::Simple;
-use Data::Dumper;
 use JSON;
 
 my $DOI_PREFIX='http://doi.org/api/handles/';
@@ -10,21 +9,26 @@ my $DOI_PREFIX='http://doi.org/api/handles/';
 # Find a link to publication by its Document Object Id.
 # param 0 -> Document Object Id
 # returns link to document.
-sub find_by_doi{
+sub find_link_by_doi{
     my $doi = $_[0];
     my $url = $DOI_PREFIX . $doi;
-    print $url . "\n";
     my $response = get $url;
     if (!$response){
         die("Error getting $url");
     }
     my $hash = decode_json($response);
-    foreach my %value ($hash->{'values'}){
-	print %value{'type'};
+    if ($hash->{'responseCode'} != 1){
+	die("Error getting $url. Code " . $hash->{'responseCode'});
     }
-    return Dumper(decode_json($response));
+    my @array= @{$hash->{'values'}};
+    my $link = '';
+    foreach my $value (@array){
+	if ($value->{'type'} eq 'URL'){
+	    $link = $value->{'data'}->{'value'};
+	    last;
+	}
+    }
+    return $link;
 }
 
-my $response = find_by_doi('10.1016/j.humpath.2008.02.014');
-#print $response . "\n";
-#1;
+1;
