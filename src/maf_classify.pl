8acbbd7d865877422f6d218682cc89e6836e697f
fanavarro <fabad@um.es>
2016-06-20 18:09:09 +0200

    Statistics on maf_classify

--- src/maf_classify.pl
+++ src/maf_classify.pl
@@ -1,6 +1,7 @@
 use myUtils::CsvManager;
 use strict;
 use warnings;
+use Scalar::Util qw(looks_like_number);
 
 #Get intput file from input parameters
 my $input = "";
@@ -42,14 +43,14 @@ $out2->myUtils::CsvManager::writeHeader();
 my $rows = $csvManager->myUtils::CsvManager::readCsv();
 $csvManager->myUtils::CsvManager::close();
 
-my @common =[];
-my @no_common = [];
-my @undefined = [];
+my @common;
+my @no_common;
+my @undefined;
 
 my $n = scalar(@{$rows});
 my $i = 1;
 foreach my $reg (@{$rows}){
-	print "$i" . "/" . "$n" . "\n";
+	# print "$i" . "/" . "$n" . "\n";
 	$i = $i + 1;
 	if ($reg->{'MINOR_ALLELE_FREQUENCY'} && $reg->{'MINOR_ALLELE_FREQUENCY'} ne '-'){
 		if($reg->{'MINOR_ALLELE_FREQUENCY'} < $maf_cut){
@@ -66,3 +67,47 @@ foreach my $reg (@{$rows}){
 }
 $out1->myUtils::CsvManager::close();
 $out2->myUtils::CsvManager::close();
+
+print "MAF >= $maf_cut \n";
+print_statistics(get_statistics(\@common));
+print "\nMAF < $maf_cut \n";
+print_statistics(get_statistics(\@no_common));
+
+# Receives a listref of entries and returns
+# average first met position, the count of
+# proteins that have a conserved frameshift
+# or not, the count of proteins that dont have
+# start codon
+sub get_statistics{
+    my $rows = $_[0];
+	my $n = scalar(@{$rows});
+	my %statistics;
+	$statistics{'average_first_met'} = 0;
+	$statistics{'count_frameshift_conserved'} = 0;
+	$statistics{'count_frameshift_no_conserved'} = 0;
+	$statistics{'count_no_met'} = 0;
+        $statistics{'total'} = $n;
+	foreach my $reg (@{$rows}){
+	    if ($reg->{'FIRST_MET_POSITION'} && looks_like_number($reg->{'FIRST_MET_POSITION'})){
+	        $statistics{'average_first_met'} += $reg->{'FIRST_MET_POSITION'};
+	    } elsif ($reg->{'FIRST_MET_POSITION'} && $reg->{'FIRST_MET_POSITION'} =~ /^\s*$/ ){
+		    $statistics{'count_no_met'} ++;
+	    }
+            if ($reg->{'READING_FRAME_STATUS'} eq 'Conserved'){
+		    $statistics{'count_frameshift_conserved'} ++;
+            } elsif($reg->{'READING_FRAME_STATUS'} eq 'Lost'){
+		    $statistics{'count_frameshift_no_conserved'} ++;
+            }
+	}
+	$statistics{'average_first_met'} = $statistics{'average_first_met'} / ($n - $statistics{'count_no_met'});
+	return \%statistics;
+}
+
+sub print_statistics{
+    my $statistics = $_[0];
+    my $n = $statistics->{'total'};
+    print "Average first met position: " . $statistics->{'average_first_met'} . "\n";
+	print "Total conserved frameshift: " . $statistics->{'count_frameshift_conserved'} . "/" . "$n (" . $statistics->{'count_frameshift_conserved'}/$n .  ")\n";
+	print "Total lost frameshift: " . $statistics->{'count_frameshift_no_conserved'} .  "/" . "$n (" . $statistics->{'count_frameshift_no_conserved'}/$n . ")\n";
+	print "Total Peptide without start codon: " . $statistics->{'count_no_met'} .  "/" . "$n (" . $statistics->{'count_no_met'}/$n . ")\n";
+}
