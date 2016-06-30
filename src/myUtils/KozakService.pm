39a4f095f09c1e4ccfcd03dd8f835d477e0db36b
fanavarro <francisco.abad@um.es>
2016-06-30 14:12:48 +0200

    KozakService null check

--- src/myUtils/KozakService.pm
+++ src/myUtils/KozakService.pm
@@ -33,18 +33,33 @@ sub get_kozak_info{
 
 	my $hash_ref_list = [];
 	my @header = qw(PREVIOUS_ATGS RELIABILITY FRAME KOZAK_IDENTITY START FINISH ORF_AMINOACID_LENGTH STOP_CODON PROTEIN_SEQUENCE);
-	# Examine first matching tables
+
+	# Initialize empty hash
+	my $empty_hash;
+	foreach my $key (@header){
+		$empty_hash->{$key} = '';
+	}
+
+	# Examine first matching table
 	my $ts = ($te->tables)[0];
-	my @rows = $ts->rows;
-	foreach my $row (@rows[1 .. scalar(@rows) - 1]) {
-		my $hash_ref;
-		for (my $i = 0; $i<scalar(@header); $i++){
-			# Delete \n from the value.
-			my $trim_value = @{$row}[$i];
-			$trim_value =~ tr/\n//d;
-			$hash_ref->{$header[$i]} = $trim_value;
+
+	# If ts is defined, construct result by retrieving table information
+	if (defined ($ts)){
+		my @rows = $ts->rows;
+		foreach my $row (@rows[1 .. scalar(@rows) - 1]) {
+			my $hash_ref;
+			for (my $i = 0; $i<scalar(@header); $i++){
+				# Delete \n from the value.
+				my $trim_value = @{$row}[$i];
+				$trim_value =~ tr/\n//d;
+				$hash_ref->{$header[$i]} = $trim_value;
+			}
+			push(@{$hash_ref_list}, $hash_ref);
 		}
-		push(@{$hash_ref_list}, $hash_ref);
+	} 
+	# If ts is not defined, return empty hash
+	else {
+		push(@{$hash_ref_list}, $empty_hash);
 	}
 	return $hash_ref_list;
 
